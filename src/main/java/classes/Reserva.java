5de40e3fbbac7cb0aa0a8db6030d416e4a4455c1
Mateus Rabelo <mateusaqles@gmail.com>
2023-06-29 00:59:13 -0300

    IMPROVE: Reserva.java class finished

--- src/main/java/classes/Reserva.java
+++ src/main/java/classes/Reserva.java
@@ -17,7 +17,66 @@
 //+ cancelarReserva(): void
 
 
-        package classes;
+package classes;
+
+import java.util.Date;
 
 public class Reserva {
+        private String passageiro, cpfPassageiro, informarcoesAdicionais;
+        private Date dataDaReserva;
+
+        public String getPassageiro()
+        {
+                return passageiro;
+        }
+
+        public void setPassageiro(String passageiro)
+        {
+                this.passageiro = passageiro;
+        }
+
+        public String getCpfPassageiro()
+        {
+                return cpfPassageiro;
+        }
+
+        public void setCpfPassageiro(String cpfPassageiro)
+        {
+                this.cpfPassageiro = cpfPassageiro;
+        }
+
+        public String getInformarcoesAdicionais()
+        {
+                return informarcoesAdicionais;
+        }
+
+        public void setInformarcoesAdicionais(String informarcoesAdicionais)
+        {
+                this.informarcoesAdicionais = informarcoesAdicionais;
+        }
+
+        public Date getDataDaReserva()
+        {
+                return dataDaReserva;
+        }
+
+        public void setDataDaReserva(Date dataDaReserva)
+        {
+                this.dataDaReserva = dataDaReserva;
+        }
+
+        public void calcularPreco()
+        {
+
+        }
+
+        public void reservar()
+        {
+
+        }
+
+        public void cancelarReserva()
+        {
+
+        }
 }
