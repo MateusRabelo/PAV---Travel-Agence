7e14aa00d5942ad7dd780e6da34aef52320002b0
Mateus Rabelo <mateusaqles@gmail.com>
2023-06-29 08:34:15 -0300

    ADD: added two new classes that inherit of Usuario class

---
+++ src/main/java/classes/Pagador.java
@@ -0,0 +1,8 @@
+// classe do UsuarioPagador, aquele responsável por pagar a passagem
+
+package classes;
+
+public class Pagador extends Usuario
+{
+
+}
---
+++ src/main/java/classes/Passageiro.java
@@ -0,0 +1,8 @@
+// classe de um usuárioPassageiro, onde serão salvos as informações do Passageiro
+
+package classes;
+
+public class Passageiro extends Usuario
+{
+
+}
