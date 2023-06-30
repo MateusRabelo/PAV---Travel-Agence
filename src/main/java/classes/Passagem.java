9a32549f94c4f4b83f0a2b0d9073235853364a28
Mateus Rabelo <mateusaqles@gmail.com>
2023-06-30 12:36:34 -0300

    UPDATE: Passagem.java class added atributes

--- src/main/java/classes/Passagem.java
+++ src/main/java/classes/Passagem.java
@@ -5,6 +5,105 @@ public class Passagem {
     private String horario;
     private int vagas;
     private double preco;
+    private String destino;
+    private String dataIda;
+    private String dataRetorno;
+    private int poltrona;
+    private int oferta;
+
+
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getHorario()
+    {
+        return horario;
+    }
+
+    public void setHorario(String horario)
+    {
+        this.horario = horario;
+    }
+
+    public int getVagas()
+    {
+        return vagas;
+    }
+
+    public void setVagas(int vagas)
+    {
+        this.vagas = vagas;
+    }
+
+    public double getPreco()
+    {
+        return preco;
+    }
+
+    public void setPreco(double preco)
+    {
+        this.preco = preco;
+    }
+
+    public String getDestino()
+    {
+        return destino;
+    }
+
+    public void setDestino(String destino)
+    {
+        this.destino = destino;
+    }
+
+    public String getDataIda()
+    {
+        return dataIda;
+    }
+
+    public void setDataIda(String dataIda)
+    {
+        this.dataIda = dataIda;
+    }
+
+    public String getDataRetorno()
+    {
+        return dataRetorno;
+    }
+
+    public void setDataRetorno(String dataRetorno)
+    {
+        this.dataRetorno = dataRetorno;
+    }
+
+    public int getPoltrona()
+    {
+        return poltrona;
+    }
+
+    public void setPoltrona(int poltrona)
+    {
+        this.poltrona = poltrona;
+    }
+
+    public int getOferta()
+    {
+        return oferta;
+    }
+
+    public void setOferta(int oferta)
+    {
+        this.oferta = oferta;
+    }
+
+
 
     public Passagem(int id, String horario, int vagas, double preco) {
         this.id = id;
@@ -12,38 +111,5 @@ public class Passagem {
         this.vagas = vagas;
         this.preco = preco;
     }
-
-    // Getters e setters
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getHorario() {
-        return horario;
-    }
-
-    public void setHorario(String horario) {
-        this.horario = horario;
-    }
-
-    public int getVagas() {
-        return vagas;
-    }
-
-    public void setVagas(int vagas) {
-        this.vagas = vagas;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
 }
+
