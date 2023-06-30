c3c10298c6ae1e43c1755cec8283fac45bf483a4
Mateus Rabelo <mateusaqles@gmail.com>
2023-06-30 14:38:42 -0300

    IMPROVE: implemented a transparent windows style

--- src/main/java/home/App.java
+++ src/main/java/home/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,6 +13,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view/Home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1050, 615);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setTitle("Agência PAV");
         stage.setScene(scene);
         stage.show();
